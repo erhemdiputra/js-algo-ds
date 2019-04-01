78d002804fd752a15ae8b25b38668d695098abea
erhemdiputra <erhem.diputra@tokopedia.com>
2019-04-02 00:09:21 +0700

    add doubly linked list shift

--- 004-doubly-linked-lists/doubly-linked-list.js
+++ 004-doubly-linked-lists/doubly-linked-list.js
@@ -47,8 +47,29 @@ class DoublyLinkedList {
         this.length--
         return poppedNode;
     }
+
+    shift() {
+        if (this.length === 0) {
+            return undefined;
+        }
+
+        let oldHead = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+
+        this.length--;
+        return oldHead;
+    }
 }
 
 let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
+list.push(3);

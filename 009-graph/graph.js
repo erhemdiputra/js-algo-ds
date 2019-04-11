9b6f028cd30681e97a6ebea74a300d2a2fa9fd9e
erhemdiputra <erhemdiputra@gmail.com>
2019-04-11 13:01:44 +0700

    add graph - add vertex

---
+++ 009-graph/graph.js
@@ -0,0 +1,13 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+}
+
+let g = new Graph();
